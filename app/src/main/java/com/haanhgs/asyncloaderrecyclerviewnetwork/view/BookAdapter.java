65fdbe2774949437b9a64d9958bf6b7d08ee2c2c
animevn <haanhth21@gmail.com>
2020-01-08 15:48:33 +0700

    ok now

--- app/src/main/java/com/haanhgs/asyncloaderrecyclerviewnetwork/view/BookAdapter.java
+++ app/src/main/java/com/haanhgs/asyncloaderrecyclerviewnetwork/view/BookAdapter.java
@@ -1,16 +1,18 @@
-package com.haanhgs.asyncloaderrecyclerviewnetwork;
+package com.haanhgs.asyncloaderrecyclerviewnetwork.view;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
+import com.haanhgs.asyncloaderrecyclerviewnetwork.R;
 import com.haanhgs.asyncloaderrecyclerviewnetwork.model.Book;
-
+import com.haanhgs.asyncloaderrecyclerviewnetwork.repo.Repo;
 import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
 
@@ -30,19 +32,26 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Book book = bookList.get(position);
+        holder.tvTitle.setText(book.getTitle());
+        holder.tvAuthor.setText(Repo.getAuthors(book.getAuthors()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return bookList == null ? 0 : bookList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.tvTitle)
+        TextView tvTitle;
+        @BindView(R.id.tvAuthor)
+        TextView tvAuthor;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
---
+++ app/src/main/java/com/haanhgs/asyncloaderrecyclerviewnetwork/view/MainActivity.java
@@ -0,0 +1,83 @@
+package com.haanhgs.asyncloaderrecyclerviewnetwork.view;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.haanhgs.asyncloaderrecyclerviewnetwork.R;
+import com.haanhgs.asyncloaderrecyclerviewnetwork.model.Constants;
+import com.haanhgs.asyncloaderrecyclerviewnetwork.repo.BookLoader;
+import com.haanhgs.asyncloaderrecyclerviewnetwork.repo.Repo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String> {
+
+    @BindView(R.id.etQuery)
+    EditText etQuery;
+    @BindView(R.id.bnSearch)
+    Button bnSearch;
+    @BindView(R.id.rvMain)
+    RecyclerView rvMain;
+
+    private Loader<String> loader;
+
+    private void initLoader(){
+        if (LoaderManager.getInstance(this).getLoader(1) != null){
+            LoaderManager.getInstance(this).initLoader(1, null, this);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initLoader();
+    }
+
+    private void handleButton(View view){
+        if (!TextUtils.isEmpty(etQuery.getText()) && Repo.isNetworkAvailable(this)){
+            Repo.hideKeyboard(this, view);
+            Bundle bundle = new Bundle();
+            bundle.putString(Constants.SEARCH_QUERY, etQuery.getText().toString());
+            LoaderManager.getInstance(this).restartLoader(1, bundle, this);
+        }
+    }
+
+    @OnClick(R.id.bnSearch)
+    public void onViewClicked(View view) {
+        handleButton(view);
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        String query = args == null ? "" : args.getString(Constants.SEARCH_QUERY);
+        return new BookLoader(this, query);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        if (data != null){
+            rvMain.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            rvMain.setAdapter(new BookAdapter(Repo.getBookList(data)));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+}
